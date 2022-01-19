1235c8d7641f0b710914773f5b166a49481e89c6
ezekielkibiego <ezekiel.nyambane@student.moringaschool.com>
2022-01-19 12:02:00 +0300

    add client_registration, login, admin login functions

--- store/views.py
+++ store/views.py
@@ -1,5 +1,114 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render,HttpResponse
+from .models import *
+from django.contrib.auth import authenticate, login, logout
+from . import *
+from datetime import date
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
 def IndexView(request):
     return render(request, 'index.html')
+
+
+def client_registration(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        # location = request.POST['location']
+        image = request.FILES['image']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+
+        if password != confirm_password:
+            passnotmatch = True
+            return render(request, "client_registration.html", {'passnotmatch':passnotmatch})
+
+        user = User.objects.create_user(username=username, email=email, password=password,first_name=first_name, last_name=last_name)
+        client = Client.objects.create(user=user, phone=phone, image=image)
+        user.save()
+        client.save()
+        alert = True
+        return render(request, "client_registration.html", {'alert':alert})
+    return render(request, "client_registration.html")
+
+def client_login(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            if request.user.is_superuser:
+                return HttpResponse("You are not a client!!")
+            else:
+                return redirect("/profile")
+        else:
+            alert = True
+            return render(request, "client_login.html", {'alert':alert})
+    return render(request, "client_login.html")
+
+def admin_login(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            if request.user.is_superuser:
+                return redirect("/add_book")
+            else:
+                return HttpResponse("You are not an admin.")
+        else:
+            alert = True
+            return render(request, "admin_login.html", {'alert':alert})
+    return render(request, "admin_login.html")
+
+def change_password(request):
+    if request.method == "POST":
+        current_password = request.POST['current_password']
+        new_password = request.POST['new_password']
+        try:
+            u = User.objects.get(id=request.user.id)
+            if u.check_password(current_password):
+                u.set_password(new_password)
+                u.save()
+                alert = True
+                return render(request, "change_password.html", {'alert':alert})
+            else:
+                currpasswrong = True
+                return render(request, "change_password.html", {'currpasswrong':currpasswrong})
+        except:
+            pass
+    return render(request, "change_password.html")
+
+def Logout(request):
+    logout(request)
+    return redirect ("/")
+
+@login_required(login_url = '/client_login')
+def profile(request):
+    return render(request, "profile.html")
+
+@login_required(login_url = '/client_login')
+def edit_profile(request):
+    client = Client.objects.get(user=request.user)
+    if request.method == "POST":
+        email = request.POST['email']
+        phone = request.POST['phone']
+        
+
+        client.user.email = email
+        client.phone = phone
+        
+        client.user.save()
+        client.save()
+        alert = True
+        return render(request, "edit_profile.html", {'alert':alert})
+    return render(request, "edit_profile.html")
+
