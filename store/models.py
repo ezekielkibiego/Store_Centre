d533f56501e81795ffab8419c6a1635184cf6358
ezekielkibiego <ezekiel.nyambane@student.moringaschool.com>
2022-01-19 11:58:49 +0300

    add client class

--- store/models.py
+++ store/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Client(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    location = models.CharField(max_length=30)
+    phone = models.CharField(max_length=10, blank=True)
+    image = models.ImageField(upload_to="", blank=True)
+
+    def __str__(self):
+        return str(self.user) + " ["+str(self.location)+']' 
+
