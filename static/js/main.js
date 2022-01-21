7afe7b97f40b0150ca9a9e34664fa1bb2698cdc2
Dhyder <victor.wairiguini@student.moringaschool.com>
2022-01-21 12:25:04 +0300

    styled login form

--- static/js/main.js
+++ static/js/main.js
@@ -236,7 +236,30 @@ jQuery(document).ready(function($) {
 	};
 	siteSticky();
 
+	// navigation
+//   var OnePageNavigation = function() {
+//     var navToggler = $('.site-menu-toggle');
+//    	$("body").on("click", ".main-menu li a[href^='#'], .smoothscroll[href^='#'], .site-mobile-menu .site-nav-wrap li a", function(e) {
+//       e.preventDefault();
 
+//       var hash = this.hash;
+	  
+//       $('html, body').animate({
+//         'scrollTop': $(hash).offset().top
+// 	}, 600, 'easeInOutExpo');
+
+// 		});
+		
+// 		$('.gototop').on('click', function() {
+			
+
+// 			$('html, body').animate({
+// 				'scrollTop': $('body').offset().top
+// 			}, 600, 'easeInOutExpo');
+// 		});
+// 	};
+// 	OnePageNavigation();
+// 	navToggler();
 
   var siteScroll = function() {
 
