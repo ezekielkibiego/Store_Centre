5d02def3158f201947e2d871c2366eaa8cb87d37
kimutaiamos <amos.kimutai@student.moringashool.com>
2022-01-21 14:07:26 +0300

    added a client

--- store/models.py
+++ store/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class Client(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     location = models.CharField(max_length=30)
-    phone = models.CharField(max_length=10, blank=True)
+    phone = models.CharField(max_length=15, blank=True)
     image = models.ImageField(upload_to="", blank=True)
 
     def __str__(self):
