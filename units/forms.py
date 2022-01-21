cc562ebbfd41fd1842ba86e3f948900a53d1c3e2
kipsang01 <enock.kipsang@student.moringaschool.com>
2022-01-21 13:14:54 +0300

    change booking form

--- units/forms.py
+++ units/forms.py
@@ -14,11 +14,11 @@ class UnitForm(forms.ModelForm):
 
 class GoodsBookingForm(forms.ModelForm):
 
-    pick_up = forms.BooleanField()
-    location = forms.CharField(max_length=50)
+    storage_type = forms.ChoiceField(choices=[('normal_storage','normal_storage'),('cold_storage','cold_storage'),('bonded_storage','bonded_storage'),('hazardious_storage','hazardious_storage'),('archive_storage','archive_storage')])
+    
     class Meta:
         model = Goods
-        fields = ('arrival_date','departure_date','description','pick_up','location')
+        fields = ('storage_type','description','arrival_date','departure_date')
         widgets = {
             'arrival_date':forms.DateInput(attrs={'type':'date'}),
             'departure_date':forms.DateInput(attrs={'type':'date'}),
