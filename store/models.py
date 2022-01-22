00e95a6bf3272990b78954d3077e5de9747e31af
kipsang01 <enock.kipsang@student.moringaschool.com>
2022-01-22 13:00:38 +0300

    add nullable image to client model

--- store/models.py
+++ store/models.py
@@ -7,7 +7,7 @@ class Client(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     location = models.CharField(max_length=30)
     phone = models.CharField(max_length=15, blank=True)
-    image = models.ImageField(upload_to="", blank=True)
+    image = models.ImageField(upload_to="", blank=True,null=True)
     def __str__(self):
         return str(self.user) + " ["+str(self.location)+']' 
 
