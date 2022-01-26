6bb338757a3422a68b2a4c526d5af00b53728bd6
kipsang01 <enock.kipsang@student.moringaschool.com>
2022-01-26 18:10:21 +0300

    change booking logics

--- units/views.py
+++ units/views.py
@@ -1,3 +1,4 @@
+import re
 from django.shortcuts import render,redirect,get_object_or_404
 from .models import *
 from transport.models import *
@@ -12,32 +13,28 @@ def book_unit(request):
     if request.method == 'POST':
         form = GoodsBookingForm(request.POST)
         if form.is_valid():
-            goods_booking = form.save(commit=False)
+            
             storage_type= form.cleaned_data.get('storage_type')
+            arrival_date= form.cleaned_data.get('arrival_date')
+            departure_date= form.cleaned_data.get('departure_date')
+            description = form.cleaned_data.get('description')
             no_of_units= form.cleaned_data.get('no_of_units') # available units
-            units = Storage.objects.filter(type=storage_type).all() # total units
-            print(no_of_units)
-            print(units)
-            print(storage_type)
+            
+            units = Storage.objects.filter(type=storage_type).first() 
             if units.available_units >= no_of_units:
-                goods_booking.owner = request.user
-
-                # goods_booking.unit_no = available_units[0]
-                # booked_unit = Unit.objects.filter(pk = available_units[0].pk ).first()
-               
-                # booked_unit.booked = True
-                # booked_unit.add_unit()
-                
-                goods_booking.save()
+                storage =  get_object_or_404(Storage,type=storage_type)
+                booked_unit =  Goods(storage_type =storage,no_of_units=no_of_units,arrival_date=arrival_date,departure_date=departure_date,description =description,owner =request.user)
+                booked_unit.add_goods()
                 storage = Storage.objects.filter(type=storage_type).first()
                 storage.available_units -= no_of_units
                 storage.add_storage() 
+                messages.success(request,f'Booked successfully')
                 
             else:
                 messages.error(request,f'No slots available in {storage_type} try again later')
                 return redirect('book')
         else: 
-            print('Not  worrkighjn')
+            messages.error(request,f'Something went wrong')
             return redirect('book')
 
         return redirect('request_transport')
@@ -53,37 +50,41 @@ def book_unit(request):
     return render(request,'booking.html',context)
 
 def dashboard(request):
-    normal_units = Storage.objects.filter(type='normal_storage')
-    booked_normal_units = Storage.objects.filter(type='normal_storage').count()
-    bonded_units = Storage.objects.filter(type='bonded_storage')
-    booked_bonded_units = Storage.objects.filter(type='bonded_storage').count()
-    archive_units = Storage.objects.filter(type='archive_storage')
-    booked_archive_units = Storage.objects.filter(type='archive_storage').count()
-    hazardious_units = Storage.objects.filter(type='hazardious_storage')
-    booked_hazardious_units = Storage.objects.filter(type='hazardious_storage').count()
-    cold_units = Storage.objects.filter(type='cold_storage')
-    booked_cold_units = Storage.objects.filter(type='cold_storage').count()
+    storages = Storage.objects.filter()
+    # booked_normal_units = Storage.objects.filter(type='normal_storage').count()
+    # bonded_units = Storage.objects.filter(type='bonded_storage')
+    # booked_bonded_units = Storage.objects.filter(type='bonded_storage').count()
+    # archive_units = Storage.objects.filter(type='archive_storage')
+    # booked_archive_units = Storage.objects.filter(type='archive_storage').count()
+    # hazardious_units = Storage.objects.filter(type='hazardious_storage')
+    # booked_hazardious_units = Storage.objects.filter(type='hazardious_storage').count()
+    # cold_units = Storage.objects.filter(type='cold_storage')
+    # booked_cold_units = Storage.objects.filter(type='cold_storage').count()
     
     if request.method == 'POST':
         form = StorageForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request,f'Booked successfully')
+        else:
+            messages.error(request,'Something went wrong')
         return redirect('dashboard')
+            
     else:
         form = StorageForm()
 
 
         args = {
-            "normal_units": normal_units,
-            "booked_normal_units":booked_normal_units,
-            "bonded_units": bonded_units,
-            "booked_bonded_units":booked_bonded_units,
-            "archive_units": archive_units,
-            "booked_archive_units":booked_archive_units,
-            "hazardious_units": hazardious_units,
-            "booked_hazardious_units":booked_hazardious_units,
-            "cold_units": cold_units,
-            "booked_cold_units":booked_cold_units,
+            "storages": storages,
+            # "booked_normal_units":booked_normal_units,
+            # "bonded_units": bonded_units,
+            # "booked_bonded_units":booked_bonded_units,
+            # "archive_units": archive_units,
+            # "booked_archive_units":booked_archive_units,
+            # "hazardious_units": hazardious_units,
+            # "booked_hazardious_units":booked_hazardious_units,
+            # "cold_units": cold_units,
+            # "booked_cold_units":booked_cold_units,
             "form":form
         }
         return render(request,'staff_dashboard.html',args)
