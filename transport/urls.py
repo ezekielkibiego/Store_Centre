52be20b8309bbf1d717bf669b179f9d7d74d0363
ezekielkibiego <ezekiel.nyambane@student.moringaschool.com>
2022-01-27 00:57:41 +0300

    deploy to heroku

--- transport/urls.py
+++ transport/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from transport import views
 
 urlpatterns=[
-    # path('request-transport/',views.request_transport,name='request_transport'),
-    # path('request-summary/',views.request_summary,name='request_summary'),
-    # path('transport-summaries/',views.summaries,name='transport_summaries'),
-    # path('payment/',views.payment,name='payment'),
+    path('request-transport/',views.request_transport,name='request_transport'),
+    path('request-summary/',views.request_summary,name='request_summary'),
+    path('transport-summaries/',views.summaries,name='transport_summaries'),
+    path('payment/',views.payment,name='payment'),
 
 ]
