34738292cf4fbfd8c0ca57bf6fc0447f6365fd02
ezekielkibiego <ezekiel.nyambane@student.moringaschool.com>
2022-01-27 00:53:33 +0300

    deploy to heroku

--- transport/views.py
+++ transport/views.py
@@ -1,75 +1,75 @@
-from django.conf import settings
-from django.shortcuts import redirect, render
-from transport.models import *
-from transport.forms import *
-from django.contrib.auth.decorators import login_required
-import requests,json
+# from django.conf import settings
+# from django.shortcuts import redirect, render
+# from transport.models import *
+# from transport.forms import *
+# from django.contrib.auth.decorators import login_required
+# import requests,json
 
-@login_required(login_url='client_login')
-def request_transport(request):
-    api_key = settings.GOOGLE_API_KEY
-    if request.method == 'POST':
-        form = TransportForm(request.POST)
-        if form.is_valid():
-            transport_request = form.save(commit=False)
-            transport_request.user = request.user
-            #client goods logic
-            goods = Goods.objects.filter(owner=request.user).last()
-            transport_request.goods= goods
-            #distance matrix logic
-            source = 'Moringa School,Nairobi,Kenya'
-            destination = transport_request.address
-            url = 'https://maps.googleapis.com/maps/api/distancematrix/json?'
-            r = requests.get(url + 'origins=' + source +
-                   '&destinations=' + destination +
-                   '&key=' + api_key)
-            x=r.json()
-            print(x)
-            distance = x['rows'][0]["elements"][0]["distance"]["value"]
-            transport_request.distance = (distance)/1000
-            #calculate price
-            price = (transport_request.distance)*200
-            transport_request.price = price
-            transport_request.save()
-            return redirect('request_summary')
-        else:
-            print(form.errors)
-    else:
-        form =TransportForm()
-    context = {
-        'form':form,
-        'api_key': api_key
-    }
-    return render(request,'request_transport.html', context)
+# @login_required(login_url='client_login')
+# def request_transport(request):
+#     api_key = settings.GOOGLE_API_KEY
+#     if request.method == 'POST':
+#         form = TransportForm(request.POST)
+#         if form.is_valid():
+#             transport_request = form.save(commit=False)
+#             transport_request.user = request.user
+#             #client goods logic
+#             goods = Goods.objects.filter(owner=request.user).last()
+#             transport_request.goods= goods
+#             #distance matrix logic
+#             source = 'Moringa School,Nairobi,Kenya'
+#             destination = transport_request.address
+#             url = 'https://maps.googleapis.com/maps/api/distancematrix/json?'
+#             r = requests.get(url + 'origins=' + source +
+#                    '&destinations=' + destination +
+#                    '&key=' + api_key)
+#             x=r.json()
+#             print(x)
+#             distance = x['rows'][0]["elements"][0]["distance"]["value"]
+#             transport_request.distance = (distance)/1000
+#             #calculate price
+#             price = (transport_request.distance)*200
+#             transport_request.price = price
+#             transport_request.save()
+#             return redirect('request_summary')
+#         else:
+#             print(form.errors)
+#     else:
+#         form =TransportForm()
+#     context = {
+#         'form':form,
+#         'api_key': api_key
+#     }
+#     return render(request,'request_transport.html', context)
 
-@login_required(login_url='client_login')
-def request_summary(request):
-    request_transport = Transport.objects.filter(user=request.user).last()
+# @login_required(login_url='client_login')
+# def request_summary(request):
+#     request_transport = Transport.objects.filter(user=request.user).last()
     
-    print(request_transport.user.first_name)
-    context = {
-        'request_transport': request_transport,
-    }
-    return render(request,'request_summary.html', context)
+#     print(request_transport.user.first_name)
+#     context = {
+#         'request_transport': request_transport,
+#     }
+#     return render(request,'request_summary.html', context)
 
 
-@login_required(login_url='client_login')
-def summaries(request):
-    summaries = Transport.objects.filter(user=request.user).all().order_by('-created')
+# @login_required(login_url='client_login')
+# def summaries(request):
+#     summaries = Transport.objects.filter(user=request.user).all().order_by('-created')
     
-    print(summaries)
-    context = {
-        'summaries': summaries,
-    }
-    return render(request,'summaries.html', context)
+#     print(summaries)
+#     context = {
+#         'summaries': summaries,
+#     }
+#     return render(request,'summaries.html', context)
 
-@login_required(login_url='client_login')
-def payment(request):
-    request_transport = Transport.objects.filter(user=request.user).last()
-    request_goods = Goods.objects.filter(owner=request.user).last()  
-    context = {
-        'request_transport': request_transport,
-        'request_goods': request_goods,
+# @login_required(login_url='client_login')
+# def payment(request):
+#     request_transport = Transport.objects.filter(user=request.user).last()
+#     request_goods = Goods.objects.filter(owner=request.user).last()  
+#     context = {
+#         'request_transport': request_transport,
+#         'request_goods': request_goods,
         
-    }
-    return render(request,'payment.html', context)
+#     }
+#     return render(request,'payment.html', context)
--- units/views.py
+++ units/views.py
@@ -1,105 +1,105 @@
-import re
-from django.shortcuts import render,redirect,get_object_or_404
-from .models import *
-from transport.models import *
-from .forms import StorageForm,GoodsBookingForm
-from django.contrib.auth.decorators import login_required
-from django.contrib import messages
+# import re
+# from django.shortcuts import render,redirect,get_object_or_404
+# from .models import *
+# from transport.models import *
+# from .forms import StorageForm,GoodsBookingForm
+# from django.contrib.auth.decorators import login_required
+# from django.contrib import messages
 
-# Create your views here.
-@login_required(login_url = '/client_login')
-def book_unit(request):
+# # Create your views here.
+# @login_required(login_url = '/client_login')
+# def book_unit(request):
     
-    if request.method == 'POST':
-        form = GoodsBookingForm(request.POST)
-        if form.is_valid():
+#     if request.method == 'POST':
+#         form = GoodsBookingForm(request.POST)
+#         if form.is_valid():
             
-            storage_type= form.cleaned_data.get('storage_type')
-            arrival_date= form.cleaned_data.get('arrival_date')
-            departure_date= form.cleaned_data.get('departure_date')
-            description = form.cleaned_data.get('description')
-            no_of_units= form.cleaned_data.get('no_of_units') # available units
+#             storage_type= form.cleaned_data.get('storage_type')
+#             arrival_date= form.cleaned_data.get('arrival_date')
+#             departure_date= form.cleaned_data.get('departure_date')
+#             description = form.cleaned_data.get('description')
+#             no_of_units= form.cleaned_data.get('no_of_units') # available units
             
-            units = Storage.objects.filter(type=storage_type).first() 
-            if units.available_units >= no_of_units:
-                storage =  get_object_or_404(Storage,type=storage_type)
-                booked_unit =  Goods(storage_type =storage,no_of_units=no_of_units,arrival_date=arrival_date,departure_date=departure_date,description =description,owner =request.user)
-                booked_unit.add_goods()
-                storage = Storage.objects.filter(type=storage_type).first()
-                storage.available_units -= no_of_units
-                storage.add_storage() 
-                messages.success(request,f'Booked successfully')
+#             units = Storage.objects.filter(type=storage_type).first() 
+#             if units.available_units >= no_of_units:
+#                 storage =  get_object_or_404(Storage,type=storage_type)
+#                 booked_unit =  Goods(storage_type =storage,no_of_units=no_of_units,arrival_date=arrival_date,departure_date=departure_date,description =description,owner =request.user)
+#                 booked_unit.add_goods()
+#                 storage = Storage.objects.filter(type=storage_type).first()
+#                 storage.available_units -= no_of_units
+#                 storage.add_storage() 
+#                 messages.success(request,f'Booked successfully')
                 
-            else:
-                messages.error(request,f'No slots available in {storage_type} try again later')
-                return redirect('book')
-        else: 
-            messages.error(request,f'Something went wrong')
-            return redirect('book')
+#             else:
+#                 messages.error(request,f'No slots available in {storage_type} try again later')
+#                 return redirect('book')
+#         else: 
+#             messages.error(request,f'Something went wrong')
+#             return redirect('book')
 
-        return redirect('request_transport')
-    else:
-        form = GoodsBookingForm()
-        storage_types = Storage.objects.all()
+#         return redirect('request_transport')
+#     else:
+#         form = GoodsBookingForm()
+#         storage_types = Storage.objects.all()
 
-    context = {
+#     context = {
         
-        "form": form,
-        "storage_types": storage_types
-    }
-    return render(request,'booking.html',context)
+#         "form": form,
+#         "storage_types": storage_types
+#     }
+#     return render(request,'booking.html',context)
 
-def dashboard(request):
-    storages = Storage.objects.filter()
-    # booked_normal_units = Storage.objects.filter(type='normal_storage').count()
-    # bonded_units = Storage.objects.filter(type='bonded_storage')
-    # booked_bonded_units = Storage.objects.filter(type='bonded_storage').count()
-    # archive_units = Storage.objects.filter(type='archive_storage')
-    # booked_archive_units = Storage.objects.filter(type='archive_storage').count()
-    # hazardious_units = Storage.objects.filter(type='hazardious_storage')
-    # booked_hazardious_units = Storage.objects.filter(type='hazardious_storage').count()
-    # cold_units = Storage.objects.filter(type='cold_storage')
-    # booked_cold_units = Storage.objects.filter(type='cold_storage').count()
+# def dashboard(request):
+#     storages = Storage.objects.filter()
+#     # booked_normal_units = Storage.objects.filter(type='normal_storage').count()
+#     # bonded_units = Storage.objects.filter(type='bonded_storage')
+#     # booked_bonded_units = Storage.objects.filter(type='bonded_storage').count()
+#     # archive_units = Storage.objects.filter(type='archive_storage')
+#     # booked_archive_units = Storage.objects.filter(type='archive_storage').count()
+#     # hazardious_units = Storage.objects.filter(type='hazardious_storage')
+#     # booked_hazardious_units = Storage.objects.filter(type='hazardious_storage').count()
+#     # cold_units = Storage.objects.filter(type='cold_storage')
+#     # booked_cold_units = Storage.objects.filter(type='cold_storage').count()
     
-    if request.method == 'POST':
-        form = StorageForm(request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request,f'Booked successfully')
-        else:
-            messages.error(request,'Something went wrong')
-        return redirect('dashboard')
+#     if request.method == 'POST':
+#         form = StorageForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             messages.success(request,f'Booked successfully')
+#         else:
+#             messages.error(request,'Something went wrong')
+#         return redirect('dashboard')
             
-    else:
-        form = StorageForm()
+#     else:
+#         form = StorageForm()
 
 
-        args = {
-            "storages": storages,
-            # "booked_normal_units":booked_normal_units,
-            # "bonded_units": bonded_units,
-            # "booked_bonded_units":booked_bonded_units,
-            # "archive_units": archive_units,
-            # "booked_archive_units":booked_archive_units,
-            # "hazardious_units": hazardious_units,
-            # "booked_hazardious_units":booked_hazardious_units,
-            # "cold_units": cold_units,
-            # "booked_cold_units":booked_cold_units,
-            "form":form
-        }
-        return render(request,'staff_dashboard.html',args)
+#         args = {
+#             "storages": storages,
+#             # "booked_normal_units":booked_normal_units,
+#             # "bonded_units": bonded_units,
+#             # "booked_bonded_units":booked_bonded_units,
+#             # "archive_units": archive_units,
+#             # "booked_archive_units":booked_archive_units,
+#             # "hazardious_units": hazardious_units,
+#             # "booked_hazardious_units":booked_hazardious_units,
+#             # "cold_units": cold_units,
+#             # "booked_cold_units":booked_cold_units,
+#             "form":form
+#         }
+#         return render(request,'staff_dashboard.html',args)
 
-def display_units(request,storage_type):
+# def display_units(request,storage_type):
 
     
-    goods = Goods.objects.filter(storage_type=storage_type)
+#     goods = Goods.objects.filter(storage_type=storage_type)
 
-    form = StorageForm()
+#     form = StorageForm()
 
-    context = {
-        "goods": goods,
-        "storage_type":storage_type,
-    }
+#     context = {
+#         "goods": goods,
+#         "storage_type":storage_type,
+#     }
 
-    return render(request,'units.html',context)
+#     return render(request,'units.html',context)
 
