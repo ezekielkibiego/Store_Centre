c63e8a46a887bf5eb979ef7d2910bb6063d92d6f
ezekielkibiego <ezekiel.nyambane@student.moringaschool.com>
2022-01-29 03:10:13 +0300

    configure email password

--- store_centre/settings.py
+++ store_centre/settings.py
@@ -200,7 +200,7 @@ AUTHENTICATION_BACKENDS = [
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'storecentre254@gmail.com' # your gmail account
-EMAIL_HOST_PASSWORD = 'EkVwJnJkEkAk254@5?'   # your  password
+EMAIL_HOST_PASSWORD = config('E_PASS')  # your  password
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
