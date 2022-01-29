3b0062d4f6091db6d30ac7bca8e27e116bfdfd2e
ezekielkibiego <ezekiel.nyambane@student.moringaschool.com>
2022-01-29 03:04:19 +0300

    add admin_profile path

--- store/urls.py
+++ store/urls.py
@@ -17,9 +17,10 @@ urlpatterns = [
 
     path("change_password/", views.change_password, name="change_password"),
     path("admin_login/", views.admin_login, name="admin_login"),
-    path('profile/', views.profile, name='profile'),
+    path('client_profile/', views.client_profile, name='client_profile'),
+    path('admin_profile/', views.admin_profile, name='admin_profile'),
+    path('staff_profile/', views.staff_profile, name='staff_profile'),
     path('update_staff_profile/',views.update_staff_profile, name='update_staff_profile'),
-
     path('accounts/profile/', views.IndexView,name='index'),
     path('update_client_profile/',views.update_client_profile, name='update_client_profile'),
     path('social-auth/',include('social_django.urls',namespace='social')),
