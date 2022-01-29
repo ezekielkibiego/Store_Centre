330c2073000e5bade6f6f621868131bfc0c160d1
ezekielkibiego <ezekiel.nyambane@student.moringaschool.com>
2022-01-29 03:05:24 +0300

    add email configs

--- store_centre/settings.py
+++ store_centre/settings.py
@@ -196,6 +196,14 @@ AUTHENTICATION_BACKENDS = [
 
    
 ]
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'storecentre254@gmail.com' # your gmail account
+EMAIL_HOST_PASSWORD = 'EkVwJnJkEkAk254@5?'   # your  password
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
 #jazzmin settings
 JAZZMIN_SETTINGS = {
       # title of the window (Will default to current_admin_site.site_title if absent or None)
