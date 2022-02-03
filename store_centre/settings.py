1ae0c3379d73e27c49e0d86b733d71a3f6293a9c
ezekielkibiego <ezekiel.nyambane@student.moringaschool.com>
2022-02-03 16:16:58 +0300

    change staticfiles

--- store_centre/settings.py
+++ store_centre/settings.py
@@ -168,12 +168,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-
-]
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
