48e104084a242b66e7cdcfb2f5aedf7dceeefbab
ezekielkibiego <ezekiel.nyambane@student.moringaschool.com>
2022-02-04 06:03:31 +0300

    deployment to heroku

--- store_centre/settings.py
+++ store_centre/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = False
-DEBUG = os.environ.get('DEBUG', False)
+DEBUG = os.environ.get('DEBUG', True)
 
 
 ALLOWED_HOSTS = ['127.0.0.1']
