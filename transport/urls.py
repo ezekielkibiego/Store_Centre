09a904198876f52db9bdccd1df51e15d41453a7b
ezekielkibiego <ezekiel.nyambane@student.moringaschool.com>
2022-02-04 05:35:35 +0300

    deploy to heroku

--- transport/urls.py
+++ transport/urls.py
@@ -3,13 +3,13 @@ from django.urls import path, include
 from transport import views
 
 urlpatterns=[
-    path('request-transport/',views.request_transport,name='request_transport'),
-    path('request-summary/',views.request_summary,name='request_summary'),
-    path('transport-summaries/',views.summaries,name='transport_summaries'),
-    path('<int:request_summary_id>/approve/',views.approval,name='approval'),
-    path('access/token', views.getAccessToken, name='get_mpesa_access_token'),
-    path('payment/',views.payment,name='payment'),
-    path('<int:request_summary_id>/approve/',views.approval,name='approval'),
-    path('access/token', views.getAccessToken, name='get_mpesa_access_token'),
+    # path('request-transport/',views.request_transport,name='request_transport'),
+    # path('request-summary/',views.request_summary,name='request_summary'),
+    # path('transport-summaries/',views.summaries,name='transport_summaries'),
+    # path('<int:request_summary_id>/approve/',views.approval,name='approval'),
+    # path('access/token', views.getAccessToken, name='get_mpesa_access_token'),
+    # path('payment/',views.payment,name='payment'),
+    # path('<int:request_summary_id>/approve/',views.approval,name='approval'),
+    # path('access/token', views.getAccessToken, name='get_mpesa_access_token'),
 
 ]
--- units/urls.py
+++ units/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<storage_type>',views.display_units,name='units'),
-    path('',views.dashboard,name='dashboard'),
-    path('book/',views.book_unit,name='book'),
+    # path('<storage_type>',views.display_units,name='units'),
+    # path('',views.dashboard,name='dashboard'),
+    # path('book/',views.book_unit,name='book'),
    
 ]
